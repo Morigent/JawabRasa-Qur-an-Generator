4c26452556ddd6168c0e788ddb76e9312c4eef47
A_Yossua <yossuagung2@gmail.com>
2026-07-19 22:22:01 +0700

    change to netlify

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   envDir: '../..',
+  build: { outDir: '../../dist' },
   plugins: [
     react(),
     tailwindcss(),
